7b2867aacfff4ef7d7f81b7547a1b40cdb3dc3a0
Simone Mosciatti <mweb.sim@gmail.com>
2017-12-18 11:08:00 +0100

    add extensions

--- build.rs
+++ build.rs
@@ -19,7 +19,15 @@ fn main() {
     cc::Build::new()
         .file("src/CDeps/SQLite/sqlite3.c")
         .include("src/CDeps/SQLite/include")
+        .define("HAVE_USLEEP", Some("1"))
+        .define("NDEBUG", Some("1"))
+        .define("HAVE_FDATASYNC", Some("1"))
         .define("SQLITE_THREADSAFE", Some("1"))
+        .define("SQLITE_ENABLE_JSON1", Some("1"))
+        .define("SQLITE_ENABLE_FTS3", Some("1"))
+        .define("SQLITE_ENABLE_FTS4", Some("1"))
+        .define("SQLITE_ENABLE_FTS5", Some("1"))
+        .define("SQLITE_ENABLE_RTREE", Some("1"))
         .compile("libsqlite3.a");
 
     #[derive(Debug)]
@@ -39,23 +47,27 @@ fn main() {
         }
     }
 
-    let bindings = bindgen::Builder::default()
-        .parse_callbacks(Box::new(SqliteTypeChooser))
-        .header("sqlite_dependencies.h")
-        .generate()
-        .expect("Unable to generate bindings for SQLite");
+    let bindings =
+        bindgen::Builder::default()
+            .parse_callbacks(Box::new(SqliteTypeChooser))
+            .header("sqlite_dependencies.h")
+            .generate()
+            .expect("Unable to generate bindings for SQLite");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings_sqlite.rs"))
+    bindings
+        .write_to_file(out_path.join("bindings_sqlite.rs"))
         .expect("Couldn't write bindings for SQLite!");
 
-    let bindings = bindgen::Builder::default()
-        .parse_callbacks(Box::new(SqliteTypeChooser))
-        .header("redis_dependencies.h")
-        .generate()
-        .expect("Unable to generate bindings for Redis");
+    let bindings =
+        bindgen::Builder::default()
+            .parse_callbacks(Box::new(SqliteTypeChooser))
+            .header("redis_dependencies.h")
+            .generate()
+            .expect("Unable to generate bindings for Redis");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings_redis.rs"))
+    bindings
+        .write_to_file(out_path.join("bindings_redis.rs"))
         .expect("Couldn't write bindings for Redis!");
 }
