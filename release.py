a681f5b0e548c3416b67c0f9263417a5f84bfb73
Simone Mosciatti <simone.mosciatti@cern.ch>
2019-02-17 16:52:21 +0100

    create simple script to build all the artifact

--- release.py
+++ release.py
@@ -18,21 +18,27 @@ try:
 except:
     pass
 
-for target in ["armv7-unknown-linux-gnueabihf", "x86_64-unknown-linux-gnu"]:
-    for feature in ["", "--features=pro", "--features=trial"]:
+arm = "armv7-unknown-linux-gnueabihf"
+intel = "x86_64-unknown-linux-gnu"
+
+pro = "--features=pro" 
+trial = "--features=trial"
+
+for target in [arm, intel]:
+    for feature in ["", pro, trial]:
         cmd = "CARGO_TARGET_DIR={} cargo build --release --target {} {}".format(scratch_space, target, feature)
         print("Executing:", cmd)
         os.system(cmd)
         result_file = "{}/{}/release/libredis_sql.so".format(scratch_space, target)
-        suffix = ""
-        if feature == "--features=pro":
+        suffix = "_v{}".format(version)
+        if feature == pro:
             suffix += "_PRO"
-        if feature == "--features=trial":
+        if feature == trial:
             suffix += "_TRIAL"
-        if target == "armv7-unknown-linux-gnueabihf":
-            suffix += "_ARM_v7"
-        if target == "x86_64-unknown-linux-gnu":
+        if target == arm:
+            suffix += "_ARMv7"
+        if target == intel:
             suffix += "_x86_64"
         shutil.copyfile(result_file, "{}/redisql{}.so".format(target_dir, suffix))
 
-os.removedirs(scratch_space)
+shutil.rmtree(scratch_space)
