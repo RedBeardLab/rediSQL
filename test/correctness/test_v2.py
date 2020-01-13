9661e62761318349c9bcc0f7e51a588a9dfcec52
Simone 'siscia' Mosciatti <simone.mosciatti@cern.ch>
2020-01-13 23:55:56 +0100

    actually add the tests -.-"

---
+++ test/correctness/test_v2.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+import unittest
+import os
+import tempfile
+import shutil
+import time
+
+import redis
+from rmtest import ModuleTestCase
+
+if "REDIS_MODULE_PATH" not in os.environ:
+    os.environ["REDIS_MODULE_PATH"] = "../../target/release/libredis_sql.so"
+
+os.environ["RUST_BACKTRACE"] = "full"
+
+class Table():
+  def __init__(self, redis, name, values, key = ""):
+    self.redis = redis
+    self.key = key
+    self.name = name
+    self.values = values
+
+  def __enter__(self):
+    create_table = "CREATE TABLE {} {}".format(self.name, self.values)
+    if self.key:
+      self.redis.client.execute_command("REDISQL.V1.EXEC", self.key, create_table) 
+    else:
+      self.redis.client.execute_command("REDISQL.V1.EXEC", create_table)
+
+  def __exit__(self, type, value, traceback):
+    drop_table = "DROP TABLE {}".format(self.name)
+    if self.key:
+      self.redis.client.execute_command("REDISQL.V1.EXEC", self.key, drop_table)
+    else:
+      self.redis.client.execute_command("REDISQL.V1.EXEC", drop_table)
+
+class DB():
+  def __init__(self, redis, key):
+    self.redis = redis
+    self.key = key
+
+  def __enter__(self):
+    self.redis.client.execute_command("REDISQL.V2.CREATE_DB", self.key)
+
+  def __exit__(self, type, value, traceback):
+    self.redis.client.execute_command("DEL", self.key)
+
+
+class TestRediSQLWithExec(ModuleTestCase('')):
+  def setUp(self):
+    self.disposable_redis = self.redis()
+
+  def tearDown(self):
+    pass
+
+  def exec_naked(self, *command):
+    return self.client.execute_command(*command)
+
+  def exec_cmd(self, *command):
+    return self.client.execute_command("REDISQL.V1.EXEC", *command)
+
+  def create_db(self, key):
+    return self.client.execute_command("REDISQL.V1.CREATE_DB", key)
+
+  def delete_db(self, key):
+    return self.client.execute_command("DEL", key)
+
+  def get_redis_server_major_version(self):
+    result = self.exec_naked("INFO", "SERVER")
+    major_version = result['redis_version'].split(':')[0].split('.')[0]
+    return int(major_version)
+
+
+class TestRediSQLCreateDB(TestRediSQLWithExec):
+  def test_create_db(self):
+    ok = self.exec_naked("REDISQL.V2.CREATE_DB", "DB")
+    self.assertEqual(ok, b'OK')
+
+if __name__ == '__main__':
+   unittest.main()
